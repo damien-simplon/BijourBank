7e616f5c5c9cb5f87530efecb336cc542553ebe3
damien-simplon <damien.bellec12@gmail.com>
2021-10-12 16:46:01 +0200

    récupération des données du form en js lors du click sur le bouton

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -4,4 +4,23 @@ console.log("Bijour Bank !");
  */
 $(document).ready(function () {
   $(document).foundation();
+
+  var formButton = document.getElementsByClassName("btSubmit").item(0);
+  formButton.onclick = function(){
+    
+    var operator = document.getElementById("operator").value;
+    var titre = document.getElementById("titre").value;
+    var desc = document.getElementById("desc").value;
+    var montant = document.getElementById("montant").value;
+
+    if(operator == ("credit" || "debit")){
+      if(operator == "credit"){
+
+      }else{
+        
+      }
+    }
+
+  }
+
 });
