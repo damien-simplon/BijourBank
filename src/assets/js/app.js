f448f9fa687cc5ea5c808d0a437cff95c8c24843
damien-simplon <damien.bellec12@gmail.com>
2021-10-13 10:47:29 +0200

    reset du js

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,24 +3,10 @@ console.log("Bijour Bank !");
  * init foundation
  */
 $(document).ready(function () {
-  $(document).foundation();
 
-  var formButton = document.getElementsByClassName("btSubmit").item(0);
-  formButton.onclick = function(){
+    $(document).foundation();
+
     
-    var operator = document.getElementById("operator").value;
-    var titre = document.getElementById("titre").value;
-    var desc = document.getElementById("desc").value;
-    var montant = document.getElementById("montant").value;
 
-    if(operator == ("credit" || "debit")){
-      if(operator == "credit"){
-
-      }else{
-        
-      }
-    }
-
-  }
 
 });
