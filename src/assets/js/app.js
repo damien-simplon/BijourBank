77a49bf4ce120419ad992bed128750eaa855139a
damien-simplon <damien.bellec12@gmail.com>
2021-10-14 16:54:10 +0200

    commencement algo localStorage

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -7,45 +7,81 @@ $(document).ready(function () {
     $(document).foundation();
     
 
-    const params = new URLSearchParams(window.location.search); // on récupére l'url
-    var values = new Array(); //initialisation d'un array
-    for (const param of params) { //parcours des paramètres de l'url récupérée
-      values[param[0]] = [param[1]]; //on affecte dans un tableau à 2 entrées le nom du param avec sa valeur
-    }
-    // pour récupérer la valeur d'un param on utilise donc values['operator'];
+    const params = new URLSearchParams(window.location.search);
+    var values = new Array();
 
-    var img = "";
-    if(values['operator'] == "credit"){
-      img = "sac-dargent";
+    localStorage.clear();
+
+    var i = 0;
+    for(var param of params){
+      values[i] = param[1];
+      i++;
+    }
+    console.log(values);
+    console.log(localStorage.getItem("tableauValues"));
+    if(typeof localStorage.getItem("tableauValues") === null){
+      values += ";";
+      localStorage.setItem("tableauValues", values);
+      console.log(localStorage.getItem("tableauValues"));
     }else{
-      img = "depenses";
+      var tableauValues = localStorage.getItem("tableauValues");
+      var tableauLength = 0;
+      tableauValues = tableauValues.split(";");
+      tableauLength = tableauValues.length - 1;
+      for( let j = 0 ; j < tableauLength ; j++ ){
+        tableauValues[j] += ";";
+      }
+      tableauValues[tableauLength] = values;
+      console.log(tableauValues);
+      localStorage.setItem("tableauValues", tableauValues);
+      var values = localStorage.getItem("tableauValues");
+      var tabLength = 0;
+      values = values.split(";");
+      valuesLength = values.length - 1;
+      console.log(values);
+      for( let j = 1 ; j < valuesLength ; j++ ){
+        values[j] = values[j].split(",");
+      }
+      console.log(values);
     }
-        
-    var html = `
-      <div class="grid-x grid-padding-x align-middle">
-        <div class="cell shrink">
-          <div class="picto">
-            <img src="./assets/images/${img}.png" alt="credit" />
-          </div>
-        </div>
-        <div class="cell auto">
-          <div>
-            <h2>${values["titre"]}</h2>
-            <small>${values["desc"]}</small>
-          </div>
-        </div>
-        <div class="cell small-3 text-right">
-          <div>
-            <p class="count">${values["montant"]}€</p>
-            <small>100%</small>
-          </div>
-        </div>
-      </div>`;
+    for(let i = 0 ; i < tabLength ; i++){
+      if(values[i][0] == "credit" || values[i][0] == "debit"){
 
-    var div = document.createElement("div");
-    div.classList.add("operation", values['operator']);
-    div.innerHTML = html;
-    var currentDiv = document.getElementsByClassName("grid-container").item(1);
-    currentDiv.append(div);
+        var img = "";
+        if(values[i][0] == "credit"){
+          img = "sac-dargent";
+        }else{
+          img = "depenses";
+        }
+            
+        var html = `
+          <div class="grid-x grid-padding-x align-middle">
+            <div class="cell shrink">
+              <div class="picto">
+                <img src="./assets/images/${img}.png" alt="credit" />
+              </div>
+            </div>
+            <div class="cell auto">
+              <div>
+                <h2>${values[i][1]}</h2>
+                <small>${values[i][2]}</small>
+              </div>
+            </div>
+            <div class="cell small-3 text-right">
+              <div>
+                <p class="count">${values[i][3]}€</p>
+                <small>100%</small>
+              </div>
+            </div>
+          </div>`;
+  
+        var div = document.createElement("div");
+        div.classList.add("operation", values['operator']);
+        div.innerHTML = html;
+        var currentDiv = document.getElementsByClassName("grid-container").item(1);
+        currentDiv.append(div);
+      }
+    }
+    
 
 });
